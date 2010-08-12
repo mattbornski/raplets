4ffd3b57466d5ce44f3b8ba357e0188945e3f92d
Matt Born <mtborn@gmail.com>
2010-08-12 16:09:36 -0700

    Test drive importing/executing raplet endpoints based on URL.

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -2,7 +2,19 @@
 
 import os.path
 import sys
+import traceback
 
 def application(environ, start_response):
-    start_response('200 OK', [('content-type', 'text/plain')])
-    return os.path.join(os.path.dirname(__file__), environ['REQUEST_URI'])
+    try:
+        request = environ['REQUEST_URI']
+        if request[0] == '/':
+            request = request[1:]
+        dir = os.path.join(os.path.dirname(__file__), request)
+        sys.path.append(dir)
+        import server
+        ret = server.application(environ, start_response)
+        sys.path.pop()
+        return ret
+    except:
+        start_response('500', [('content-type', 'text/plain')])
+        return traceback.format_exc()
